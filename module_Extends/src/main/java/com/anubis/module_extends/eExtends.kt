d274cca1a1bc8c8f1ad6bcb5dac4c88b5a2e4f34
AnubisASN <anubisasn@gmail.com>
2021-12-28 18:24:07 +0800

    Update eExtends.kt

--- module_Extends/src/main/java/com/anubis/module_extends/eExtends.kt
+++ module_Extends/src/main/java/com/anubis/module_extends/eExtends.kt
@@ -179,6 +179,41 @@ fun eLogE(hint: Any? = "", e: Any? = null, TAG: String = "TAGe") {
 }
 
 /**
+ * if替换------------------------------------------------------------------------------------
+ */
+inline infix fun Boolean.trueLet(trueBlock: Boolean.() -> Unit): Else {
+    if (this) {
+        trueBlock()
+        return NotDoElse(this)
+    }
+    return DoElse(this)
+}
+
+inline infix fun Boolean.falseLet(falseBlock: Boolean.() -> Unit): Else {
+    if (!this) {
+        falseBlock()
+        return NotDoElse(this)
+    }
+    return DoElse(this)
+}
+
+interface Else {
+    infix fun elseLet(elseBlock: Boolean.() -> Unit)
+}
+
+class DoElse(private val boolean: Boolean) : Else {
+    override infix fun elseLet(elseBlock: Boolean.() -> Unit) {
+        elseBlock(boolean)
+    }
+}
+
+class NotDoElse(private val boolean: Boolean) : Else {
+    override infix fun elseLet(elseBlock: Boolean.() -> Unit) {
+    }
+}
+
+
+/**
  * 错误输出
  */
 fun eErrorOut(e: Any?): String? {
@@ -3822,6 +3857,5 @@ open class eReflection internal constructor() {
         cls.getDeclaredMethod(methodName, String::class.java)
     }
 }
-
 
 
